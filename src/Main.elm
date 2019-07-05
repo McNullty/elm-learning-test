32312c0427f2a72451541e7c654616e6def3f2af
Mladen Čikara <mladen.cikara@gmail.com>
2019-07-05 22:32:58 +0200

    Formatting

--- src/Main.elm
+++ src/Main.elm
@@ -1,12 +1,13 @@
 module Main exposing (main)
 
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Bootstrap.Grid as Grid
 import Bootstrap.Button as Button
 import Bootstrap.Form as Form
 import Bootstrap.Form.Input as Input
+import Bootstrap.Grid as Grid
 import Browser
+import Html exposing (..)
+import Html.Attributes exposing (..)
+
 
 type alias User =
     { name : String
@@ -14,6 +15,7 @@ type alias User =
     , password : String
     , loggedIn : Bool
     }
+
 
 initialModel : User
 initialModel =
@@ -23,40 +25,43 @@ initialModel =
     , loggedIn = False
     }
 
+
 view : User -> Html msg
 view user =
     Grid.container []
         [ Grid.row []
             [ Grid.col []
-                [h1 [] [ text "Sign up" ]
+                [ h1 [] [ text "Sign up" ]
                 , Form.form []
-                 [ Form.group []
-                     [ Form.label [ for "name" ] [ text "Name" ]
-                     , Input.text [ Input.id "name" ]
-                     , Form.help [] [ text "Your name." ]
-                     ]
-                 , Form.group []
-                     [ Form.label [ for "email" ] [ text "Email" ]
-                     , Input.email [ Input.id "email", Input.attrs [ placeholder "test@example.com" ] ]
-                     , Form.help [] [ text "Your email." ]
-                     ]
-                 , Form.group []
-                     [ Form.label [ for "password" ] [ text "Password" ]
-                     , Input.password [ Input.id "password" ]
-                     ]
-                 ,  Button.button [ Button.primary ] [ text "Create my account" ]
-                 ]
+                    [ Form.group []
+                        [ Form.label [ for "name" ] [ text "Name" ]
+                        , Input.text [ Input.id "name" ]
+                        , Form.help [] [ text "Your name." ]
+                        ]
+                    , Form.group []
+                        [ Form.label [ for "email" ] [ text "Email" ]
+                        , Input.email [ Input.id "email", Input.attrs [ placeholder "test@example.com" ] ]
+                        , Form.help [] [ text "Your email." ]
+                        ]
+                    , Form.group []
+                        [ Form.label [ for "password" ] [ text "Password" ]
+                        , Input.password [ Input.id "password" ]
+                        ]
+                    , Button.button [ Button.primary ] [ text "Create my account" ]
+                    ]
                 ]
             ]
         ]
+
 
 update : msg -> User -> User
 update msg model =
     initialModel
 
+
 main =
     Browser.sandbox
-            { init = initialModel
-            , update = update
-            , view = view
-            }
+        { init = initialModel
+        , update = update
+        , view = view
+        }
