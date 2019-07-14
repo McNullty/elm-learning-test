da8e235f0ed622da7323d9873a9b2feef7aec4fe
Mladen Čikara <mladen.cikara@gmail.com>
2019-07-14 13:35:47 +0200

    Decoding json object to Elm record

---
+++ src/DecodingJson.elm
@@ -0,0 +1,168 @@
+module DecodingJson exposing (..)
+
+import Browser exposing (Document)
+import Html exposing (Html, button, div, h3, table, td, text, th, tr)
+import Html.Events exposing (onClick)
+import Http exposing (Response)
+import Json.Decode exposing (Decoder, field, int, list, map3, string)
+import Result exposing (Result)
+
+type alias Post =
+    { id : Int
+    , title : String
+    , author : String
+    }
+
+type alias Model =
+    { posts: List Post
+    , errorMessage : Maybe String
+    }
+
+
+type Msg
+    = SendHttpRequest
+    | DataReceived (Result Http.Error (List Post))
+
+
+view : Model -> Document Msg
+view model =
+    { title = "Http get example"
+    , body =
+        [ div []
+            [ button [ onClick SendHttpRequest ]
+                [ text "Get data from server" ]
+            , viewPostsOrError model
+            ]
+        ]
+    }
+
+
+viewPostsOrError : Model -> Html Msg
+viewPostsOrError model =
+    case model.errorMessage of
+        Just message ->
+            viewError message
+
+        Nothing ->
+            viewPosts model.posts
+
+
+viewError : String -> Html Msg
+viewError errorMessage =
+    let
+        errorHeading =
+            "Couldn't fetch data at this time."
+    in
+        div []
+            [ h3 [] [ text errorHeading ]
+            , text ("Error: " ++ errorMessage)
+            ]
+
+
+viewPosts : List Post -> Html Msg
+viewPosts posts =
+    div []
+        [ h3 [] [ text "Posts" ]
+        , table []
+            ([ viewTableHeader ] ++ List.map viewPost posts)
+        ]
+
+
+viewTableHeader : Html Msg
+viewTableHeader =
+    tr []
+        [ th []
+            [ text "ID" ]
+        , th []
+            [ text "Title" ]
+        , th []
+            [ text "Author" ]
+        ]
+
+
+viewPost : Post -> Html Msg
+viewPost post =
+    tr []
+        [ td []
+            [ text (String.fromInt post.id) ]
+        , td []
+            [ text post.title ]
+        , td []
+            [ text post.author ]
+        ]
+
+
+postDecoder : Decoder Post
+postDecoder =
+    map3 Post
+        (field "id" int)
+        (field "title" string)
+        (field "author" string)
+
+
+httpCommand : Cmd Msg
+httpCommand =
+    Http.get
+        { url = "http://localhost:5019/posts"
+        , expect = Http.expectJson DataReceived (list postDecoder)
+        }
+
+
+createErrorMessageFromHttpError : Http.Error -> String
+createErrorMessageFromHttpError httpError =
+    case httpError of
+        Http.BadUrl message ->
+            message
+
+        Http.Timeout ->
+            "Server is taking too long to respond. Please try again later."
+
+        Http.NetworkError ->
+            "It appears you don't have an Internet connection right now."
+
+        Http.BadStatus response ->
+            String.fromInt response
+
+        Http.BadBody response ->
+            response
+
+
+update : Msg -> Model -> ( Model, Cmd Msg)
+update msg model =
+    case msg of
+        SendHttpRequest ->
+            ( model, httpCommand)
+
+        DataReceived (Ok posts) ->
+            ( { model
+                | posts = posts
+                , errorMessage = Nothing
+              }
+            , Cmd.none
+            )
+
+        DataReceived (Err httpError) ->
+            ( { model
+                | errorMessage = Just (createErrorMessageFromHttpError httpError)
+              }
+            , Cmd.none
+            )
+
+
+init : flag -> ( Model, Cmd Msg )
+init _ =
+    ( { posts = []
+      , errorMessage = Nothing
+      }
+    , Cmd.none
+    )
+
+
+main : Program () Model Msg
+main =
+    Browser.document
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
