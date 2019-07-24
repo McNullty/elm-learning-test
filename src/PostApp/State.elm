94df63562ee636436f9b341ee2bb8fd5c7184870
Mladen Čikara <mladen.cikara@gmail.com>
2019-07-24 20:18:29 +0200

    Refactor State module

--- src/PostApp/State.elm
+++ src/PostApp/State.elm
@@ -1,7 +1,7 @@
 module PostApp.State exposing (..)
 
 import Browser
-import Browser.Navigation exposing (Key, load, pushUrl)
+import Browser.Navigation as Nav
 import PostApp.Rest exposing (fetchPostsCommand)
 import PostApp.Types exposing (..)
 import RemoteData
@@ -25,16 +25,16 @@ update msg model =
                 Nothing ->
                     (model, Cmd.none)
                 Just _ ->
-                    ( model, pushUrl model.key (Url.toString url) )
+                    ( model, Nav.pushUrl model.key (Url.toString url) )
 
         LinkClicked (Browser.External href) ->
-            (model, load href)
+            (model, Nav.load href)
 
         UrlChanged url ->
             ({model | url = url}, Cmd.none)
 
 
-init : flag -> Url -> Key -> ( Model, Cmd Msg )
+init : flag -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
 init _ url key =
     ( { posts = RemoteData.Loading
       , key = key
