b1bf627f064181077f43920249dced56524bc755
Mladen Čikara <mladen.cikara@gmail.com>
2019-07-28 15:27:20 +0200

    Link for returning to list of posts

--- src/PostApp/Views/Edit.elm
+++ src/PostApp/Views/Edit.elm
@@ -1,13 +1,14 @@
 module PostApp.Views.Edit exposing (view)
 
-import Html exposing (Html, br, button, div, h3, input, text)
-import Html.Attributes exposing (type_, value)
+import Html exposing (Html, a, br, button, div, h3, input, text)
+import Html.Attributes exposing (href, type_, value)
 import PostApp.Types exposing (Msg, Post)
 
 view : Post -> Html Msg
 view post =
     div []
-        [ h3 [] [ text "Edit Post"]
+        [ a [ href "/posts" ] [ text "Back" ]
+        , h3 [] [ text "Edit Post"]
         , editForm post
         ]
 
