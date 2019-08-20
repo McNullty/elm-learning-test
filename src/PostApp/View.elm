9446a48908b92e1e3323ef609e0a0d14d3ffb494
Mladen Čikara <mladen.cikara@gmail.com>
2019-08-20 19:19:52 +0200

    Adding bootstrap header

--- src/PostApp/View.elm
+++ src/PostApp/View.elm
@@ -1,7 +1,11 @@
 module PostApp.View exposing (..)
 
+import Bootstrap.CDN as CDN
+import Bootstrap.Grid as Grid
+import Bootstrap.Grid.Col as Col
 import Browser exposing (Document)
 import Html exposing (Html, h3, text)
+import Html.Attributes exposing (class)
 import PostApp.Misc exposing (findPostById)
 import PostApp.Routing as Route
 import PostApp.Types as Types exposing (WebData)
@@ -19,29 +23,38 @@ view model =
 
 viewBody : Types.Model -> Html Types.Msg
 viewBody model =
-    case model.route of
-        Route.PostsRoute ->
-            List.view model
+    Grid.container []
+            [ CDN.stylesheet -- creates an inline style node with the Bootstrap CSS. TODO: Remove before deploying
+            , Grid.row []
+                [ Grid.col [ Col.md6, Col.offsetMd3 ]
+                     [
+                        case model.route of
+                            Route.PostsRoute ->
+                                List.view model
 
-        Route.PostRoute postId ->
-            case findPostById postId model.posts of
-                Just post ->
-                    Edit.view post
+                            Route.PostRoute postId ->
+                                case findPostById postId model.posts of
+                                    Just post ->
+                                        Edit.view post
 
-                Nothing ->
-                    waitingForResponseView
+                                    Nothing ->
+                                        waitingForResponseView
 
-        Route.NewPostRoute ->
-            New.view
+                            Route.NewPostRoute ->
+                                New.view
 
-        Route.NotFoundRoute ->
-            notFoundView
+                            Route.NotFoundRoute ->
+                                notFoundView
+                     ]
+                ]
+            ]
+
 
 
 notFoundView : Html msg
 notFoundView =
-    h3 [] [ text "Oops! The page you requested was not found!" ]
+    h3 [ class "text-center" ] [ text "Oops! The page you requested was not found!" ]
 
 waitingForResponseView : Html msg
 waitingForResponseView =
-    h3 [] [ text "Waiting for response from servers!" ]
+    h3 [ class "text-center" ] [ text "Waiting for response from servers!" ]
