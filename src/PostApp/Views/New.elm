600e3394d9fde6dc1615bbe3d4c7b5559f4ccc95
Mladen Čikara <mladen.cikara@gmail.com>
2019-08-20 22:31:41 +0200

    Refactor: New view to bootstrap

--- src/PostApp/Views/New.elm
+++ src/PostApp/Views/New.elm
@@ -1,14 +1,17 @@
 module PostApp.Views.New exposing (..)
 
-import Html exposing (Html, a, br, button, div, h3, input, text)
-import Html.Attributes exposing (href, type_)
-import Html.Events exposing (onClick, onInput)
+import Bootstrap.Alert as Alert
+import Bootstrap.Button as Button exposing (button, onClick)
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Html exposing (Html, div, h3, text)
+import Html.Attributes exposing (for, href)
 import PostApp.Types exposing (Msg(..))
 
 view : Html Msg
 view =
     div []
-        [ a [ href "/posts" ] [ text "Back" ]
+        [ Alert.link [ href "/posts" ] [ text "Back" ]
         , h3 [] [ text "Create New Post" ]
         , newPostForm
         ]
@@ -17,38 +20,31 @@ view =
 newPostForm : Html Msg
 newPostForm =
     div []
-        [ div []
-            [ text "Title"
-            , br [] []
-            , input
-                [ type_ "text"
-                , onInput NewPostTitle
+        [ Form.form []
+            [ Form.group []
+                [ Form.label [ for "title" ] [ text "Title" ]
+                , Input.text
+                      [ Input.id "title"
+                      , Input.onInput NewPostTitle
+                      ]
                 ]
-                []
-            ]
-        , br [] []
-        , div []
-            [ text "Author Name"
-            , br [] []
-            , input
-                [ type_ "text"
-                , onInput NewAuthorName
+            , Form.group []
+                [ Form.label [ for "authorName" ] [ text "Author name" ]
+                , Input.text
+                    [ Input.id "authorName"
+                    , Input.onInput NewAuthorName
+                    ]
                 ]
-                []
-            ]
-        , br [] []
-        , div []
-            [ text "Author URL"
-            , br [] []
-            , input
-                [ type_ "text"
-                , onInput NewAuthorUrl
+            , Form.group []
+                [ Form.label [ for "authorUrl" ] [ text "Author URL" ]
+                , Input.text
+                    [ Input.id "authorUrl"
+                    , Input.onInput NewAuthorUrl
+                    ]
                 ]
-                []
             ]
-        , br [] []
         , div []
-            [ button [ onClick CreateNewPost ]
-                [ text "Submit" ]
+            [ button [ onClick CreateNewPost, Button.large, Button.primary ]
+                [text "Submit"]
             ]
         ]
