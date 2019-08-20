34e6065e1b9a0dffdeaacccc9cbee3eff0aa77dc
Mladen Čikara <mladen.cikara@gmail.com>
2019-08-20 22:24:10 +0200

    Refactor: Edit view to bootstrap

--- src/PostApp/Views/Edit.elm
+++ src/PostApp/Views/Edit.elm
@@ -1,14 +1,17 @@
 module PostApp.Views.Edit exposing (view)
 
-import Html exposing (Html, a, br, button, div, h3, input, text)
-import Html.Attributes exposing (href, type_, value)
-import Html.Events exposing (onClick, onInput)
+import Bootstrap.Alert as Alert
+import Bootstrap.Button as Button exposing (button, onClick)
+import Bootstrap.Form as Form
+import Bootstrap.Form.Input as Input
+import Html exposing (Html, div, h3, text)
+import Html.Attributes exposing (for, href)
 import PostApp.Types exposing (Msg(..), Post)
 
 view : Post -> Html Msg
 view post =
     div []
-        [ a [ href "/posts" ] [ text "Back" ]
+        [ Alert.link [ href "/posts" ] [ text "Back" ]
         , h3 [] [ text "Edit Post"]
         , editForm post
         ]
@@ -17,41 +20,34 @@ view post =
 editForm : Post -> Html Msg
 editForm post =
     div []
-        [ div []
-            [ text "Title"
-            , br [] []
-            , input
-                [ type_ "text"
-                , value post.title
-                , onInput (UpdateTitle post.id)
+        [ Form.form []
+            [ Form.group []
+                [ Form.label [ for "title" ] [ text "Title" ]
+                , Input.text
+                      [ Input.id "title"
+                      , Input.value post.title
+                      , Input.onInput (UpdateTitle post.id)
+                      ]
                 ]
-                []
-            ]
-        , br [] []
-        , div []
-            [ text "Author name"
-            , br [] []
-            , input
-                [ type_ "text"
-                , value post.author.name
-                , onInput (UpdateAuthorName post.id)
+            , Form.group []
+                [ Form.label [ for "authorName" ] [ text "Author name" ]
+                , Input.text
+                    [ Input.id "authorName"
+                    , Input.value post.author.name
+                    , Input.onInput (UpdateAuthorName post.id)
+                    ]
                 ]
-                []
-            ]
-        , br [] []
-        , div []
-            [ text "Author URL"
-            , br [] []
-            , input
-                [ type_ "text"
-                , value post.author.url
-                , onInput (UpdateAuthorUrl post.id)
+            , Form.group []
+                [ Form.label [ for "authorUrl" ] [ text "Author URL" ]
+                , Input.text
+                    [ Input.id "authorUrl"
+                    , Input.value post.author.url
+                    , Input.onInput (UpdateAuthorUrl post.id)
+                    ]
                 ]
-                []
             ]
-        , br [] []
         , div []
-            [ button [ onClick (SubmitUpdatedPost post.id)]
+            [ button [ onClick (SubmitUpdatedPost post.id), Button.large, Button.primary ]
                 [text "Submit"]
             ]
         ]
