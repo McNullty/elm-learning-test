fe20f61766009d7a1ad5049abea86d10cc4ef1d1
Mladen Čikara <mladen.cikara@gmail.com>
2019-08-20 14:16:42 +0200

    Patch example is working as expected, no redirects

--- src/PatchExample.elm
+++ src/PatchExample.elm
@@ -20,8 +20,7 @@ type alias Post =
 type Msg
     = UpdateTitle Int String
     | PatchPost Int
---    | PostUpdated (RemoteData Http.Error Post)
---    | DataReceived (Result Http.Error String)
+    | PostUpdated (RemoteData Http.Error Post)
 
 
 init : flag -> ( Post, Cmd Msg )
@@ -50,65 +49,36 @@ view post =
 
 url : String
 url =
-    "http://localhost:5019/posts"
+    "http://localhost:5019/posts/"
 
 
---createErrorMessageFromHttpError : Http.Error -> String
---createErrorMessageFromHttpError httpError =
---    case httpError of
---        Http.BadUrl message ->
---            message
---
---        Http.Timeout ->
---            "Server is taking too long to respond. Please try again later."
---
---        Http.NetworkError ->
---            "It appears you don't have an Internet connection right now."
---
---        Http.BadStatus response ->
---            String.fromInt response
---
---        Http.BadBody response ->
---            response
+postEncoder : Post -> Encode.Value
+postEncoder post =
+    Encode.object
+        [ ( "id", Encode.int post.id )
+        , ( "title", Encode.string post.title )
+        ]
 
---postEncoder : Post -> Encode.Value
---postEncoder post =
---    Encode.object
---        [ ( "id", Encode.int post.id )
---        , ( "title", Encode.string post.title )
---        ]
---
---postDecoder : Decoder Post
---postDecoder =
---    Decode.succeed Post
---        |> required "id" int
---        |> required "title" string
---
---
---updatePostCommand : Post -> Cmd Msg
---updatePostCommand post =
---    Http.request
---        { method = "PATCH"
---        , headers = []
---        , url = "http://localhost:5019/posts/" ++ (String.fromInt post.id)
---        , body = Http.jsonBody (postEncoder post)
---        , expect = Http.expectJson (RemoteData.fromResult >> PostUpdated) postDecoder
---        , timeout = Nothing
---        , tracker = Nothing
---        }
 
---update : Msg -> Model -> ( Model, Cmd Msg )
---update msg model =
---    case msg of
---        SendHttpRequest post ->
---            ( model, updatePostCommand post)
---
---        DataReceived (Ok title) ->
---            ( { model | title = title }, Cmd.none )
---
---        DataReceived (Err error) ->
---            ( { model | errorMessage = Just (createErrorMessageFromHttpError error) }, Cmd.none )
---
+postDecoder : Decoder Post
+postDecoder =
+    Decode.succeed Post
+        |> required "id" int
+        |> required "title" string
+
+
+updatePostCommand : Post -> Cmd Msg
+updatePostCommand post =
+    Http.request
+        { method = "PATCH"
+        , headers = []
+        , url = url ++ (String.fromInt post.id)
+        , body = Http.jsonBody (postEncoder post)
+        , expect = Http.expectJson (RemoteData.fromResult >> PostUpdated) postDecoder
+        , timeout = Nothing
+        , tracker = Nothing
+        }
+
 
 update : Msg -> Post -> ( Post, Cmd Msg )
 update msg post =
@@ -117,9 +87,11 @@ update msg post =
         _ = Debug.log "Post: " post
     in
     case msg of
-        UpdateTitle id title ->
+        UpdateTitle _ title ->
             ( { post | title = title }, Cmd.none)
-        PatchPost id ->
+        PatchPost _ ->
+            (post, updatePostCommand post)
+        PostUpdated _ ->
             (post, Cmd.none)
 
 main : Program () Post Msg
